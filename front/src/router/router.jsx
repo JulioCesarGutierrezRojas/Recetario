5c75ae5015b003ba9a0fd14fb377cee7f463952e
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-03-16 20:31:53 -0600

    ADD: Navbar

--- front/src/router/router.jsx
+++ front/src/router/router.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
 import Login from "../modules/auth/views/Login";
+import NavBar from "../components/NavBar.jsx";
 
 const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const location = useLocation();
+    const isAdmin = localStorage.getItem("role") === "admin";
+
+    return (
+        <>
+            { location.pathname !== "/" && <NavBar isAdmin={isAdmin} /> }
+            <Routes>
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </>
+    );
 };
 
 export default AppRouter;
