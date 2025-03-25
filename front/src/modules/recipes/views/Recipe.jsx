89ec84bd25f9ae45467ab58c21e8fec5b2cd7b4b
JaelSantos01 <20223TN110@utez.edu.mx>
2025-03-25 12:51:26 -0600

    Home realizado, falta la parte del navigate para el recipes

---
+++ front/src/modules/recipes/views/Recipe.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../../components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useNavigate } from "react-router-dom";
+
+const Recipe = () => {
+    return(
+        <>
+        <Navbar/>
+            <div className="container-fluid">
+
+            </div>
+        </>
+    )
+}
+
+export default Recipe;
--- front/src/router/router.jsx
+++ front/src/router/router.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "../modules/auth/views/Login";
+import Home from "../modules/recipes/views/Home";
+import Recipe from "../modules/recipes/views/Recipe";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/recipe" element={<Recipe />} />
       </Routes>
     </BrowserRouter>
   );
