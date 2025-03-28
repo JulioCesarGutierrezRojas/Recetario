b0c1b8566db6d6c44f93e688d8db46dafb54f050
AnaRonco14 <20223tn147@utez.edu.mx>
2025-03-27 18:53:52 -0600

    prefijo: ADD

--- back/comments/views.py
+++ back/comments/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Comment
+from .serializers import CommentSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('created_at')
+    serializer_class = CommentSerializer
 
 # Create your views here.
