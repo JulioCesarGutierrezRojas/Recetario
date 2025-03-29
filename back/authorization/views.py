98dbdef67b234dacb4b0330855b5ad918b4c4737
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-03-29 17:25:11 -0600

    ADD: module auth finish back

--- back/authorization/views.py
+++ back/authorization/views.py
@@ -4,9 +4,13 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import LoginDTO
+from .serializers import LoginDTO, ResetPasswordDTO, SendEmailRecoverDTO, VerifyTokenDTO
 from .utils import send_recovery_email, generate_token
-from users.models import User, Token, Role
+from users.models import User, Token
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -19,6 +23,9 @@ def login(request):
 
         found_user = User.objects.filter(email=user.validated_data['email']).first()
 
+        if not found_user.is_active:
+            return Response({'error': 'Usuario inactivo'}, status=status.HTTP_400_BAD_REQUEST)
+
         if found_user and found_user.check_password(user.validated_data['password']):
             token = AccessToken.for_user(found_user)
             token['email'] = found_user.email
@@ -29,55 +36,63 @@ def login(request):
 
         return Response({'error': 'Credenciales Invalidas'}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        print(e)
+        logger.error(f"Error en el login: {e}")
         return Response({'error': 'Ha ocurrido un error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def send_email_recover(request):
     try:
-        email = request.data.get('email')
+        email = SendEmailRecoverDTO(data=request.data)
 
-        if not email:
-            return Response({'error': 'El correo es requerido'}, status=status.HTTP_400_BAD_REQUEST)
+        if not email.is_valid():
+            return Response(email.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        user_found = User.objects.filter(email=email).first()
-
-        if not user_found:
-            return Response({'error': 'No existe un usuario con ese correo'}, status=status.HTTP_400_BAD_REQUEST)
-
-        Token.objects.filter(user=user_found).delete()
+        user_found = User.objects.filter(email=email.validated_data['email']).first()
 
         token, expiration = generate_token()
+        Token.objects.filter(user=user_found).delete()
         Token.objects.create(user=user_found, token=token, date_expiration=expiration)
 
-        send_recovery_email(email,token, user_found.name)
+        send_recovery_email(email.validated_data['email'],token, user_found.name)
 
         return Response({'message': 'Se te ha enviado un correo para recuperar tu contraseña'}, status=status.HTTP_200_OK)
     except Exception as e:
-        print(e)
+        logger.error(f"Error al enviar el correo de recuperación: {e}")
         return Response({'error': 'Ha ocurrido un error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def verify_token(request):
     try:
-        token = request.data.get('token')
+        token = VerifyTokenDTO(data=request.data)
 
-        if not token or not email:
-            return Response({'error': 'El token y el correo son requeridos'}, status=status.HTTP_400_BAD_REQUEST)
+        if not token.is_valid():
+            return Response(token.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        user_found = User.objects.filter(email=email).first()
+        token_obj = token.validated_data['token_obj']
 
-        if not user_found:
-            return Response({'error': 'No existe un usuario con ese correo'}, status=status.HTTP_400_BAD_REQUEST)
-
-        token_found = Token.objects.filter(user=user_found, token=token).first()
-
-        if not token_found:
-            return Response({'error': 'Token invalido o expirado'}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({'message': 'Token valido'}, status=status.HTTP_200_OK)
+        return Response({'data': token_obj.user.id, 'message': 'Verificación exitosa'}, status=status.HTTP_200_OK)
     except Exception as e:
-        print(e)
+        logger.error(f"Error al verificar el token: {e}")
+        return Response({'error': 'Ha ocurrido un error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def change_password(request):
+    try:
+        credentials = ResetPasswordDTO(data=request.data)
+
+        if not credentials.is_valid():
+            return Response(credentials.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user_found = User.objects.get(id=credentials.validated_data['user'])
+
+        user_found.set_password(credentials.validated_data['new_password'])
+        user_found.save()
+        Token.objects.filter(user=user_found).delete()
+
+        return Response({'message': 'Contraseña restablecida correctamente'}, status=status.HTTP_200_OK)
+    except Exception as e:
+        logger.error(f"Error al cambiar la contraseña: {e}")
         return Response({'error': 'Ha ocurrido un error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
