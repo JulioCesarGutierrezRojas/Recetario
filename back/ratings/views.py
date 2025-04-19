415c7eebb8f21b0f0c82f0f5e34f56e13d14e9ae
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-04-19 00:08:08 -0600

    UPDATE: get_queryset in comments and ratings

--- back/ratings/views.py
+++ back/ratings/views.py
@@ -7,4 +7,15 @@ class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all().order_by('created_at')
     serializer_class = RatingSerializer
 
+    def get_queryset(self):
+        user_param = self.request.query_params.get('user')
+
+        if user_param == 'me':
+            return self.queryset.filter(user=self.request.user)
+
+        if user_param:
+            return self.queryset.filter(user_id=user_param)
+
+        return self.queryset.all()
+
 # Create your views here.
